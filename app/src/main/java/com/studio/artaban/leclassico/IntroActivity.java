b03d8de26f3055bb3a5646760fe9ba02c5212c4c
v-pascal <pascal.viguie@studio-artaban.com>
2016-07-18 16:44:29 +0200

    Controls panel & concert stage added

--- app/src/main/java/com/studio/artaban/leclassico/IntroActivity.java
+++ app/src/main/java/com/studio/artaban/leclassico/IntroActivity.java
@@ -1,5 +1,7 @@
 package com.studio.artaban.leclassico;
 
+import android.animation.ObjectAnimator;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.v7.app.AppCompatActivity;
 
 import android.support.v4.app.Fragment;
@@ -10,9 +12,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.view.animation.TranslateAnimation;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.studio.artaban.leclassico.components.CurvedViewPager;
+import com.studio.artaban.leclassico.components.LimitlessViewPager;
 import com.studio.artaban.leclassico.helpers.Logs;
 
 /**
@@ -21,7 +26,7 @@ import com.studio.artaban.leclassico.helpers.Logs;
  */
 public class IntroActivity extends AppCompatActivity {
 
-    private static final String DATA_KEY_ALPHA = "alpha";
+    private static final String DATA_KEY_INTRO_DISPLAYED = "introDisplayed";
     // Data keys
 
     private static final int INTRO_PAGE_COUNT = 5;
@@ -48,13 +53,15 @@ public class IntroActivity extends AppCompatActivity {
                                  Bundle savedInstanceState) {
 
             Logs.add(Logs.Type.V, null);
-            View rootView = inflater.inflate(R.layout.fragment_intro, container, false);
+            View rootView = (getArguments().getInt(DATA_KEY_POSITION) == 0)?
+                    inflater.inflate(R.layout.fragment_welcome, container, false):
+                    inflater.inflate(R.layout.fragment_intro, container, false);
 
             // Assign left & right page tag
             if (getArguments().getInt(DATA_KEY_POSITION) == 0)
-                rootView.setTag(CurvedViewPager.TAG_LEFT_PAGE);
+                rootView.setTag(LimitlessViewPager.TAG_PAGE_LEFT_TOP);
             else if (getArguments().getInt(DATA_KEY_POSITION) == (INTRO_PAGE_COUNT - 1))
-                rootView.setTag(CurvedViewPager.TAG_RIGHT_PAGE);
+                rootView.setTag(LimitlessViewPager.TAG_PAGE_RIGHT_BOTTOM);
 
 
 
@@ -63,9 +70,11 @@ public class IntroActivity extends AppCompatActivity {
 
 
 
-            TextView framePosition = (TextView) rootView.findViewById(R.id.section_label);
-            framePosition.setText("#" + getArguments().getInt(DATA_KEY_POSITION));
-            //mFrameImage = (ImageView)rootView.findViewById(R.id.frame_image);
+            if (getArguments().getInt(DATA_KEY_POSITION) != 0) {
+                TextView framePosition = (TextView) rootView.findViewById(R.id.section_label);
+                framePosition.setText("#" + getArguments().getInt(DATA_KEY_POSITION));
+                //mFrameImage = (ImageView)rootView.findViewById(R.id.frame_image);
+            }
 
 
 
@@ -84,6 +93,7 @@ public class IntroActivity extends AppCompatActivity {
 
 
 
+
             /*
             Bitmap bitmap = openBitmapFile(getArguments().getInt(DATA_KEY_SYNCHRO_OFFSET),
                     getArguments().getBoolean(DATA_KEY_SYNCHRO_LOCAL));
@@ -93,12 +103,43 @@ public class IntroActivity extends AppCompatActivity {
 
 
 
+
+
         }
     }
 
+    private void skipIntro() { // Display connection layout
+
+        Logs.add(Logs.Type.V, null);
+        CoordinatorLayout layout = ((CoordinatorLayout) mViewPager.getParent());
+        layout.clearAnimation();
+        layout.setTranslationX(0);
+        TranslateAnimation anim = new TranslateAnimation(0f, -mViewPager.getWidth(), 0f, 0f);
+        anim.setFillAfter(true);
+        anim.setDuration(300);
+        layout.startAnimation(anim);
+
+        mIntroDisplayed = true;
+    }
+
     //
-    private CurvedViewPager mViewPager;
-    private float mAlphaFab;
+    private LimitlessViewPager mViewPager;
+    private boolean mIntroDisplayed;
+
+    public void onNextStep(View sender) {
+
+        Logs.add(Logs.Type.V, "sender: " + sender);
+        if (mViewPager.getCurrentItem() < (INTRO_PAGE_COUNT - 1))
+            mViewPager.setCurrentItem(mViewPager.getCurrentItem() + 1, true);
+
+        else
+            skipIntro();
+    }
+    public void onSkipIntro(View sender) {
+
+        Logs.add(Logs.Type.V, "sender: " + sender);
+        skipIntro();
+    }
 
     //////
     @Override
@@ -109,33 +150,22 @@ public class IntroActivity extends AppCompatActivity {
 
         // Restore data
         if (savedInstanceState != null)
-            mAlphaFab = savedInstanceState.getFloat(DATA_KEY_ALPHA);
+            mIntroDisplayed = savedInstanceState.getBoolean(DATA_KEY_INTRO_DISPLAYED, false);
 
+        // Check to display intro
+        if (mIntroDisplayed) {
+            ((CoordinatorLayout)findViewById(R.id.main_content)).setVisibility(View.GONE);
+            return;
+        }
 
+        final ImageButton skip = (ImageButton)findViewById(R.id.image_skip);
+        final ImageView step1 = (ImageView)findViewById(R.id.image_step_1);
+        final ImageView step2 = (ImageView)findViewById(R.id.image_step_2);
+        final ImageView step3 = (ImageView)findViewById(R.id.image_step_3);
+        final ImageView step4 = (ImageView)findViewById(R.id.image_step_4);
 
-
-
-
-
-        /*
-        final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        fab.setAlpha(mAlphaFab);
-        */
-
-
-
-
-
-
-
-        mViewPager = (CurvedViewPager) findViewById(R.id.container);
+        // Set up the ViewPager
+        mViewPager = (LimitlessViewPager) findViewById(R.id.container);
         mViewPager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
 
             @Override
@@ -149,28 +179,9 @@ public class IntroActivity extends AppCompatActivity {
             }
         });
         mViewPager.setPageTransformer(false, new ViewPager.PageTransformer() {
-            @Override
-            public void transformPage(View page, float position) {
 
-                if (position <= 0) // This page is moving out to the left
-                    page.setAlpha(1.f + (position * 2.f));
-                else if (position <= 1) // This page is moving in from the right
-                    page.setAlpha(1.f - (position * 2.f));
-            }
-        });
-        mViewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-
-            @Override
-            public void onPageSelected(int position) {
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int state) {
-            }
-
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-                if (position == (INTRO_PAGE_COUNT - 2)) {
+            private void scroll(View page, float position) { // Apply a scrolling to the concert stage images
+                if ((mViewPager.getCurrentItem() == 0) && (position < 0f)) {
 
 
 
@@ -178,10 +189,8 @@ public class IntroActivity extends AppCompatActivity {
 
 
 
-                    mAlphaFab = positionOffset;
-                    //fab.setAlpha(positionOffset);
-
-
+                    ImageView ball = (ImageView)page.findViewById(R.id.image_ball);
+                    ball.setTranslationX(position * 120f);
 
 
 
@@ -190,13 +199,171 @@ public class IntroActivity extends AppCompatActivity {
 
                 }
             }
+
+            @Override
+            public void transformPage(View page, float position) {
+
+                if (position <= 0f) { // This page is moving out to the left
+
+                    scroll(page, position);
+                    page.setAlpha(1f + (position * 2f));
+
+                } else if (position <= 1f) { // This page is moving in from the right
+                    page.setAlpha(1f - (position * 2f));
+                }
+            }
+        });
+        mViewPager.setOnLimitlessListener(new LimitlessViewPager.OnLimitCrossedListener() {
+
+            private static final float RATIO_ROTATION = 0.06f;
+            private float mRotation;
+
+            private static final float RATIO_START_BEHAVIOR = 0.25f;
+            // Start to translate ViewPager when this % of the screen width is reached
+            private static final float RATIO_DISPLAY_CONNECT = 0.4f;
+            // Display connection layout when this % of the screen width is reached
+
+            private boolean mScrolling; // Right page scrolling flag
+            private float mTranslate;
+
+            @Override
+            public boolean onStartBehavior(boolean leftTop, float originX, float originY) {
+                if ((!leftTop) && (originX > (mViewPager.getWidth() - (mViewPager.getWidth() *
+                        RATIO_START_BEHAVIOR)))) {
+
+                    mTranslate = 0f;
+                    mScrolling = true;
+                    return true;
+                }
+                mRotation = 0f;
+                return false;
+            }
+
+            @Override
+            public boolean onFinishBehavior(boolean leftTop) {
+
+                View page = mViewPager.findViewWithTag((leftTop) ?
+                        LimitlessViewPager.TAG_PAGE_LEFT_TOP : LimitlessViewPager.TAG_PAGE_RIGHT_BOTTOM);
+                if (leftTop) {
+
+                    if ((mRotation > 0f) || (mRotation < 0f)) {
+
+                        ObjectAnimator animation = ObjectAnimator.ofFloat(page, "rotationY",
+                                mRotation, 0f);
+                        animation.setDuration(500);
+                        animation.start();
+                    }
+                    return false;
+                }
+                if (!mScrolling)
+                    return false;
+
+                if (mTranslate < 0f) {
+
+                    CoordinatorLayout layout = ((CoordinatorLayout) page.getParent().getParent());
+                    layout.clearAnimation();
+                    layout.setTranslationX(0);
+                    TranslateAnimation anim;
+                    if (mTranslate < (-mViewPager.getWidth() * RATIO_DISPLAY_CONNECT)) {
+
+                        anim = new TranslateAnimation(mTranslate, -mViewPager.getWidth(), 0f, 0f);
+                        anim.setFillAfter(true);
+                        anim.setDuration(200);
+
+                        mIntroDisplayed = true;
+
+                    } else {
+                        anim = new TranslateAnimation(mTranslate, 0f, 0f, 0f);
+                        anim.setDuration(500);
+                    }
+                    layout.startAnimation(anim);
+                }
+                mScrolling = false;
+                return true;
+            }
+
+            @Override
+            public boolean onLeftTopLimitCrossed(View page, float deltaX, float deltaY) {
+                if (deltaX > 0f) {
+
+                    mRotation = deltaX * RATIO_ROTATION;
+                    page.setRotationY(mRotation);
+                }
+                return false;
+            }
+
+            @Override
+            public boolean onRightBottomLimitCrossed(View page, float deltaX, float deltaY) {
+                if (!mScrolling)
+                    return false;
+
+                if (deltaX < 0f) {
+
+                    mTranslate += deltaX;
+                    ((CoordinatorLayout) page.getParent().getParent()).setTranslationX(mTranslate);
+                }
+                return true;
+            }
+        });
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+
+            private static final float ALPHA_NO_SELECTION = 0.3f;
+
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                switch (position) {
+                    case 0: { // 0 -> 1
+
+                        skip.setAlpha(positionOffset);
+                        step1.setAlpha(positionOffset);
+                        step2.setAlpha(positionOffset * ALPHA_NO_SELECTION);
+                        step3.setAlpha(positionOffset * ALPHA_NO_SELECTION);
+                        step4.setAlpha(positionOffset * ALPHA_NO_SELECTION);
+                        break;
+                    }
+                    case 1: { // 1 -> 2
+
+                        if (positionOffset > 0f) {
+                            step1.setAlpha((positionOffset * (ALPHA_NO_SELECTION - 1f)) + 1f);
+                            step2.setAlpha((positionOffset * (1f - ALPHA_NO_SELECTION)) + ALPHA_NO_SELECTION);
+                        }
+                        break;
+                    }
+                    case 2: { // 2 -> 3
+
+                        if (positionOffset > 0f) {
+                            step2.setAlpha((positionOffset * (ALPHA_NO_SELECTION - 1f)) + 1f);
+                            step3.setAlpha((positionOffset * (1f - ALPHA_NO_SELECTION)) + ALPHA_NO_SELECTION);
+                        }
+                        break;
+                    }
+                    case 3: { // 3 -> 4
+
+                        if (positionOffset > 0f) {
+                            step3.setAlpha((positionOffset * (ALPHA_NO_SELECTION - 1f)) + 1f);
+                            step4.setAlpha((positionOffset * (1f - ALPHA_NO_SELECTION)) + ALPHA_NO_SELECTION);
+                        }
+                        break;
+                    }
+                }
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
         });
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
 
-        outState.putFloat(DATA_KEY_ALPHA, mAlphaFab);
+        outState.putBoolean(DATA_KEY_INTRO_DISPLAYED, mIntroDisplayed);
 
         Logs.add(Logs.Type.V, "outState: " + outState);
         super.onSaveInstanceState(outState);
---
+++ app/src/main/java/com/studio/artaban/leclassico/components/LimitlessViewPager.java
@@ -0,0 +1,143 @@
+package com.studio.artaban.leclassico.components;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+import com.studio.artaban.leclassico.data.Constants;
+import com.studio.artaban.leclassico.helpers.Logs;
+
+/**
+ * Created by pascal on 17/07/16.
+ * LimitlessViewPager component class:
+ * ViewPager with unbounded behaviour, accessing limit crossed position when the user has reached the
+ * left/top or right/bottom limits.
+ */
+public class LimitlessViewPager extends ViewPager {
+
+    public static final String TAG_PAGE_LEFT_TOP = "leftTop";
+    public static final String TAG_PAGE_RIGHT_BOTTOM = "rightBottom";
+    // Tags
+
+    public interface OnLimitCrossedListener {
+
+        boolean onStartBehavior(boolean leftTop, float originX, float originY);
+        boolean onFinishBehavior(boolean leftTop);
+
+        boolean onLeftTopLimitCrossed(View page, float deltaX, float deltaY);
+        boolean onRightBottomLimitCrossed(View page, float deltaX, float deltaY);
+    };
+    // Listener interface
+
+    public LimitlessViewPager(Context context) {
+        super(context);
+    }
+
+    public LimitlessViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    //
+    private OnLimitCrossedListener mListener;
+
+    public void setOnLimitlessListener(OnLimitCrossedListener listener) {
+        mListener = listener;
+    }
+
+    //
+    private static final String ERROR_INVALID_PAGE_TAG = "Tag of the left/top or right/bottom pages not defined";
+    private static final String ERROR_LISTENER_NOT_DEFINED = "'OnLimitCrossedListener' not defined";
+
+    private int mPointerId = Constants.NO_DATA;
+    private float mPrevX;
+    private float mPrevY;
+
+    //////
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if ((getCurrentItem() == 0)||(getCurrentItem() == (getAdapter().getCount() - 1))) {
+            switch (event.getActionMasked()) {
+
+                case MotionEvent.ACTION_DOWN: {
+                    if (mPointerId == Constants.NO_DATA) {
+
+                        Logs.add(Logs.Type.I, "Start unbounded scroll");
+                        mPointerId = event.getPointerId(0);
+                        mPrevX = event.getX(0);
+                        mPrevY = event.getY(0);
+
+                        if (mListener != null) {
+
+                            // Check page tag definition
+                            View page = (getCurrentItem() == 0)?
+                                    findViewWithTag(TAG_PAGE_LEFT_TOP):findViewWithTag(TAG_PAGE_RIGHT_BOTTOM);
+                            if (page == null) {
+
+                                Logs.add(Logs.Type.F, ERROR_INVALID_PAGE_TAG);
+                                mPointerId = Constants.NO_DATA;
+                                break;
+                            }
+                            if (mListener.onStartBehavior(getCurrentItem() == 0, mPrevX, mPrevY))
+                                return true;
+
+                        } else {
+
+                            Logs.add(Logs.Type.F, ERROR_LISTENER_NOT_DEFINED);
+                            mPointerId = Constants.NO_DATA;
+                        }
+                    }
+                    break;
+                }
+                case MotionEvent.ACTION_UP: {
+                    if (mPointerId == Constants.NO_DATA)
+                        break;
+
+                    for (int i = 0; i < event.getPointerCount(); ++i) {
+                        if (event.getPointerId(i) == mPointerId) {
+
+                            Logs.add(Logs.Type.I, "Finish unbounded scroll");
+                            mPointerId = Constants.NO_DATA;
+                            mPrevX = 0f;
+                            mPrevY = 0f;
+
+                            if (mListener.onFinishBehavior(getCurrentItem() == 0))
+                                return true;
+                            break;
+                        }
+                    }
+                    break;
+                }
+                case MotionEvent.ACTION_MOVE: {
+                    if (mPointerId == Constants.NO_DATA)
+                        break;
+
+                    for (int i = 0; i < event.getPointerCount(); ++i) {
+                        if (event.getPointerId(i) == mPointerId) {
+
+                            // Get page according left or right pages
+                            View page = (getCurrentItem() == 0)?
+                                    findViewWithTag(TAG_PAGE_LEFT_TOP):findViewWithTag(TAG_PAGE_RIGHT_BOTTOM);
+
+                            int deltaX = (int)(event.getX(i) - mPrevX);
+                            int deltaY = (int)(event.getY(i) - mPrevY);
+                            if (getCurrentItem() == 0) {
+                                if (mListener.onLeftTopLimitCrossed(page, deltaX, deltaY))
+                                    return true;
+
+                            } else {
+
+                                if (mListener.onRightBottomLimitCrossed(page, deltaX, deltaY))
+                                    return true;
+                            }
+                            break;
+                        }
+                    }
+                    break;
+                }
+            }
+        }
+        return super.onTouchEvent(event);
+    }
+}
