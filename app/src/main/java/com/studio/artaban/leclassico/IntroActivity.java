ee826c4cb036e6bfd7b63f8c64fa4ad0bef7427d
v-pascal <pascal.viguie@studio-artaban.com>
2016-07-19 15:37:31 +0200

    Concert stage replaced by factory behavior

--- app/src/main/java/com/studio/artaban/leclassico/IntroActivity.java
+++ app/src/main/java/com/studio/artaban/leclassico/IntroActivity.java
@@ -1,6 +1,9 @@
 package com.studio.artaban.leclassico;
 
 import android.animation.ObjectAnimator;
+import android.annotation.SuppressLint;
+import android.content.res.Configuration;
+import android.graphics.Point;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.v7.app.AppCompatActivity;
 
@@ -15,9 +18,11 @@ import android.view.ViewGroup;
 import android.view.animation.TranslateAnimation;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.studio.artaban.leclassico.components.LimitlessViewPager;
+import com.studio.artaban.leclassico.data.Constants;
 import com.studio.artaban.leclassico.helpers.Logs;
 
 /**
@@ -27,6 +32,12 @@ import com.studio.artaban.leclassico.helpers.Logs;
 public class IntroActivity extends AppCompatActivity {
 
     private static final String DATA_KEY_INTRO_DISPLAYED = "introDisplayed";
+
+    private static final String DATA_KEY_ALPHA_SKIP = "alphaSkip";
+    private static final String DATA_KEY_ALPHA_STEP1 = "alphaStep1";
+    private static final String DATA_KEY_ALPHA_STEP2 = "alphaStep2";
+    private static final String DATA_KEY_ALPHA_STEP3 = "alphaStep3";
+    private static final String DATA_KEY_ALPHA_STEP4 = "alphaStep4";
     // Data keys
 
     private static final int INTRO_PAGE_COUNT = 5;
@@ -34,10 +45,15 @@ public class IntroActivity extends AppCompatActivity {
     //
     public static class PlaceholderFragment extends Fragment {
 
-        public static PlaceholderFragment newInstance(int position) {
+        public static final String DATA_KEY_POSITION = "position";
+        public static final String DATA_KEY_SCREEN_HEIGHT = "height";
+        // Data keys
+
+        public static Fragment newInstance(int position, int height) {
 
             Bundle args = new Bundle();
-            args.putInt(DATA_KEY_POSITION, position);
+            args.putInt(PlaceholderFragment.DATA_KEY_POSITION, position);
+            args.putInt(PlaceholderFragment.DATA_KEY_SCREEN_HEIGHT, height);
 
             PlaceholderFragment fragment = new PlaceholderFragment();
             fragment.setArguments(args);
@@ -45,7 +61,61 @@ public class IntroActivity extends AppCompatActivity {
         }
 
         //
-        private static final String DATA_KEY_POSITION = "position";
+        private float getSizeRatio() { // Return size ratio for all representation images
+
+            int backHeight = getArguments().getInt(DATA_KEY_SCREEN_HEIGHT);
+            if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT)
+                backHeight >>= 1; // Half height for portrait
+            else // Include layout padding for landscape
+                backHeight -= getResources().getDimensionPixelSize(R.dimen.intro_padding_bottom);
+
+            // Include background image padding
+            backHeight -= getResources().getDimensionPixelSize(R.dimen.activity_vertical_margin) << 1;
+
+            return (float)backHeight / (float)Constants.INTRO_BACKGROUND_IMAGE_HEIGHT;
+        }
+        private void position(View root) { // Position the representation images
+
+
+
+
+
+            if (getArguments().getInt(DATA_KEY_POSITION) != 0)
+                return;
+
+
+
+
+            //int pixel = getResources().getDimensionPixelSize(R.dimen.appbar_padding_top);
+            //Logs.add(Logs.Type.I, "p: " + pixel);
+
+            /*
+            ImageView back = (ImageView)root.findViewById(R.id.image_background);
+            Logs.add(Logs.Type.I, "w: " + ((RelativeLayout.LayoutParams)back.getLayoutParams()).width);
+            Logs.add(Logs.Type.I, "h: " + ((RelativeLayout.LayoutParams)back.getLayoutParams()).height);
+            */
+
+
+
+
+
+
+            float sizeRatio = getSizeRatio();
+
+
+            Logs.add(Logs.Type.I, "ratio: " + sizeRatio);
+
+
+            ImageView container = (ImageView)root.findViewById(R.id.image_container);
+            ((RelativeLayout.LayoutParams)container.getLayoutParams()).width =
+                    (int)(Constants.INTRO_CONTAINER_IMAGE_HEIGHT * sizeRatio);
+
+
+
+
+
+
+        }
 
         //////
         @Override
@@ -53,9 +123,16 @@ public class IntroActivity extends AppCompatActivity {
                                  Bundle savedInstanceState) {
 
             Logs.add(Logs.Type.V, null);
+
+
+
+
+
+
+
             View rootView = (getArguments().getInt(DATA_KEY_POSITION) == 0)?
-                    inflater.inflate(R.layout.fragment_welcome, container, false):
-                    inflater.inflate(R.layout.fragment_intro, container, false);
+                    inflater.inflate(R.layout.fragment_intro, container, false):
+                    inflater.inflate(R.layout.fragment_temp, container, false);
 
             // Assign left & right page tag
             if (getArguments().getInt(DATA_KEY_POSITION) == 0)
@@ -63,6 +140,7 @@ public class IntroActivity extends AppCompatActivity {
             else if (getArguments().getInt(DATA_KEY_POSITION) == (INTRO_PAGE_COUNT - 1))
                 rootView.setTag(LimitlessViewPager.TAG_PAGE_RIGHT_BOTTOM);
 
+            position(rootView);
 
 
 
@@ -123,8 +201,28 @@ public class IntroActivity extends AppCompatActivity {
     }
 
     //
-    private LimitlessViewPager mViewPager;
-    private boolean mIntroDisplayed;
+    private LimitlessViewPager mViewPager; // Introduction view pager component
+    private boolean mIntroDisplayed; // Introduction display flag
+
+    private float mAlphaSkip;
+    private float mAlphaStep1;
+    private float mAlphaStep2;
+    private float mAlphaStep3;
+    private float mAlphaStep4;
+    // Controls panel alpha
+
+    private void applyAlpha(ImageView skip, ImageView step1, ImageView step2, ImageView step3,
+                            ImageView step4) {
+
+        //Logs.add(Logs.Type.V, "skip: " + skip + "step1: " + step1 + "step2: " + step2 +
+        //        "step3: " + step3 + "step4: " + step4);
+
+        skip.setAlpha(mAlphaSkip);
+        step1.setAlpha(mAlphaStep1);
+        step2.setAlpha(mAlphaStep2);
+        step3.setAlpha(mAlphaStep3);
+        step4.setAlpha(mAlphaStep4);
+    }
 
     public void onNextStep(View sender) {
 
@@ -149,8 +247,16 @@ public class IntroActivity extends AppCompatActivity {
         setContentView(R.layout.activity_intro);
 
         // Restore data
-        if (savedInstanceState != null)
-            mIntroDisplayed = savedInstanceState.getBoolean(DATA_KEY_INTRO_DISPLAYED, false);
+        if (savedInstanceState != null) {
+
+            mIntroDisplayed = savedInstanceState.getBoolean(DATA_KEY_INTRO_DISPLAYED);
+
+            mAlphaSkip = savedInstanceState.getFloat(DATA_KEY_ALPHA_SKIP);
+            mAlphaStep1 = savedInstanceState.getFloat(DATA_KEY_ALPHA_STEP1);
+            mAlphaStep2 = savedInstanceState.getFloat(DATA_KEY_ALPHA_STEP2);
+            mAlphaStep3 = savedInstanceState.getFloat(DATA_KEY_ALPHA_STEP3);
+            mAlphaStep4 = savedInstanceState.getFloat(DATA_KEY_ALPHA_STEP4);
+        }
 
         // Check to display intro
         if (mIntroDisplayed) {
@@ -158,13 +264,18 @@ public class IntroActivity extends AppCompatActivity {
             return;
         }
 
+        // Set up controls panel
         final ImageButton skip = (ImageButton)findViewById(R.id.image_skip);
         final ImageView step1 = (ImageView)findViewById(R.id.image_step_1);
         final ImageView step2 = (ImageView)findViewById(R.id.image_step_2);
         final ImageView step3 = (ImageView)findViewById(R.id.image_step_3);
         final ImageView step4 = (ImageView)findViewById(R.id.image_step_4);
+        applyAlpha(skip, step1, step2, step3, step4);
 
         // Set up the ViewPager
+        final Point screenSize = new Point();
+        getWindowManager().getDefaultDisplay().getSize(screenSize);
+
         mViewPager = (LimitlessViewPager) findViewById(R.id.container);
         mViewPager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
 
@@ -175,13 +286,15 @@ public class IntroActivity extends AppCompatActivity {
 
             @Override
             public Fragment getItem(int position) {
-                return PlaceholderFragment.newInstance(position);
+                return PlaceholderFragment.newInstance(position, screenSize.y);
             }
         });
         mViewPager.setPageTransformer(false, new ViewPager.PageTransformer() {
 
-            private void scroll(View page, float position) { // Apply a scrolling to the concert stage images
-                if ((mViewPager.getCurrentItem() == 0) && (position < 0f)) {
+            private void scroll(boolean toTheLeft, View page, float position) {
+            // Apply a scrolling to the representation images
+
+                if (((toTheLeft) && (position < 0f)) || (((!toTheLeft) && (position < 1f)))) {
 
 
 
@@ -189,8 +302,11 @@ public class IntroActivity extends AppCompatActivity {
 
 
 
-                    ImageView ball = (ImageView)page.findViewById(R.id.image_ball);
-                    ball.setTranslationX(position * 120f);
+
+
+                    //ImageView ball = (ImageView)page.findViewById(R.id.image_ball);
+                    //ball.setTranslationX(position * 120f);
+
 
 
 
@@ -203,19 +319,32 @@ public class IntroActivity extends AppCompatActivity {
             @Override
             public void transformPage(View page, float position) {
 
+
+
+
+
+
                 if (position <= 0f) { // This page is moving out to the left
 
-                    scroll(page, position);
-                    page.setAlpha(1f + (position * 2f));
+                    scroll(true, page, position);
+                    //page.setAlpha(1f + (position * 2f));
 
                 } else if (position <= 1f) { // This page is moving in from the right
-                    page.setAlpha(1f - (position * 2f));
+
+                    scroll(false, page, position);
+                    //page.setAlpha(1f - (position * 2f));
                 }
+
+
+
+
+
+
             }
         });
         mViewPager.setOnLimitlessListener(new LimitlessViewPager.OnLimitCrossedListener() {
 
-            private static final float RATIO_ROTATION = 0.06f;
+            private static final float RATIO_ROTATION = 0.05f / 800f;
             private float mRotation;
 
             private static final float RATIO_START_BEHAVIOR = 0.25f;
@@ -286,7 +415,8 @@ public class IntroActivity extends AppCompatActivity {
             public boolean onLeftTopLimitCrossed(View page, float deltaX, float deltaY) {
                 if (deltaX > 0f) {
 
-                    mRotation = deltaX * RATIO_ROTATION;
+                    float screenRatio = mViewPager.getWidth() * RATIO_ROTATION;
+                    mRotation = deltaX * screenRatio;
                     page.setRotationY(mRotation);
                 }
                 return false;
@@ -314,38 +444,39 @@ public class IntroActivity extends AppCompatActivity {
                 switch (position) {
                     case 0: { // 0 -> 1
 
-                        skip.setAlpha(positionOffset);
-                        step1.setAlpha(positionOffset);
-                        step2.setAlpha(positionOffset * ALPHA_NO_SELECTION);
-                        step3.setAlpha(positionOffset * ALPHA_NO_SELECTION);
-                        step4.setAlpha(positionOffset * ALPHA_NO_SELECTION);
+                        mAlphaSkip = positionOffset;
+                        mAlphaStep1 = positionOffset;
+                        mAlphaStep2 = positionOffset * ALPHA_NO_SELECTION;
+                        mAlphaStep3 = positionOffset * ALPHA_NO_SELECTION;
+                        mAlphaStep4 = positionOffset * ALPHA_NO_SELECTION;
                         break;
                     }
                     case 1: { // 1 -> 2
 
                         if (positionOffset > 0f) {
-                            step1.setAlpha((positionOffset * (ALPHA_NO_SELECTION - 1f)) + 1f);
-                            step2.setAlpha((positionOffset * (1f - ALPHA_NO_SELECTION)) + ALPHA_NO_SELECTION);
+                            mAlphaStep1 = (positionOffset * (ALPHA_NO_SELECTION - 1f)) + 1f;
+                            mAlphaStep2 = (positionOffset * (1f - ALPHA_NO_SELECTION)) + ALPHA_NO_SELECTION;
                         }
                         break;
                     }
                     case 2: { // 2 -> 3
 
                         if (positionOffset > 0f) {
-                            step2.setAlpha((positionOffset * (ALPHA_NO_SELECTION - 1f)) + 1f);
-                            step3.setAlpha((positionOffset * (1f - ALPHA_NO_SELECTION)) + ALPHA_NO_SELECTION);
+                            mAlphaStep2 = (positionOffset * (ALPHA_NO_SELECTION - 1f)) + 1f;
+                            mAlphaStep3 = (positionOffset * (1f - ALPHA_NO_SELECTION)) + ALPHA_NO_SELECTION;
                         }
                         break;
                     }
                     case 3: { // 3 -> 4
 
                         if (positionOffset > 0f) {
-                            step3.setAlpha((positionOffset * (ALPHA_NO_SELECTION - 1f)) + 1f);
-                            step4.setAlpha((positionOffset * (1f - ALPHA_NO_SELECTION)) + ALPHA_NO_SELECTION);
+                            mAlphaStep3 = (positionOffset * (ALPHA_NO_SELECTION - 1f)) + 1f;
+                            mAlphaStep4 = (positionOffset * (1f - ALPHA_NO_SELECTION)) + ALPHA_NO_SELECTION;
                         }
                         break;
                     }
                 }
+                applyAlpha(skip, step1, step2, step3, step4);
             }
 
             @Override
@@ -364,6 +495,12 @@ public class IntroActivity extends AppCompatActivity {
     protected void onSaveInstanceState(Bundle outState) {
 
         outState.putBoolean(DATA_KEY_INTRO_DISPLAYED, mIntroDisplayed);
+
+        outState.putFloat(DATA_KEY_ALPHA_SKIP, mAlphaSkip);
+        outState.putFloat(DATA_KEY_ALPHA_STEP1, mAlphaStep1);
+        outState.putFloat(DATA_KEY_ALPHA_STEP2, mAlphaStep2);
+        outState.putFloat(DATA_KEY_ALPHA_STEP3, mAlphaStep3);
+        outState.putFloat(DATA_KEY_ALPHA_STEP4, mAlphaStep4);
 
         Logs.add(Logs.Type.V, "outState: " + outState);
         super.onSaveInstanceState(outState);
